62b4379bb22e19331be9f4b2dbc759019b6feed2
Donbosco1995 <donfdo5@gmail.com>
2024-01-02 16:55:03 +0530

    first commit

---
+++ todo-app/src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+// import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task, deleteTodo, editTodo, markTodo, toggleComplete}) => {
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+          <input type = "checkbox" className="mark-icon"  onClick={() => markTodo(task.id)}/>
+        {/* <FontAwesomeIcon className="mark-icon" icon={faCheckSquare} onClick={() => markTodo(task.id)} /> */}
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)}  />
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  )
+}
